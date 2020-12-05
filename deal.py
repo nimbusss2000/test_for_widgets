e53a884d32c5c1e220c568a88559f7114de8d07c
Ekaterina <eugryumova@team.amocrm.com>
2020-12-05 19:48:37 +0300

    create quick deal

---
+++ deal.py
@@ -0,0 +1,7 @@
+
+class Deal:
+
+    def __init__(self, deal_name, contact_name, company_name):
+        self.deal_name = deal_name
+        self.contact_name = contact_name
+        self.company_name = company_name
